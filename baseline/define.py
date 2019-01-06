16918ba765b0d9597c085dcc9a511dbcfc5b8efa
imaharu <light1200out@gmail.com>
2019-01-06 22:25:13 +0900

    Data

--- baseline/define.py
+++ baseline/define.py
@@ -7,25 +7,19 @@ import pickle
 import argparse
 from preprocessing import *
 
-# Set logger
-from logging import getLogger, StreamHandler, DEBUG
-logger = getLogger(__name__)
-handler = StreamHandler()
-handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
-logger.addHandler(handler)
-logger.propagate = False
-
-
 ##### args #####
 parser = argparse.ArgumentParser(description='Sequence to Sequence Model by using Pytorch')
-'''train details'''
+
+'''
+    epoch
+    embed_size
+    hidden_size
+    dropout
+    batch_size
+'''
+
 parser.add_argument('--epoch', '-e', type=int, default=35,
                     help='Number of sweeps over the dataset to train')
-
-'''train_num embed hidden batch'''
-parser.add_argument('--train_doc_num','-t', type=int,
-                    help='train num')
 parser.add_argument('--embed_size', type=int, default=256,
                     help='size of embed size for word representation')
 parser.add_argument('--dropout', type=int, default=0.2,
@@ -34,60 +28,78 @@ parser.add_argument('--hidden_size', type=int, default=256,
                     help='number of hidden units')
 parser.add_argument('--batch_size', '-b', type=int, default=50,
                     help='Number of batchsize')
-parser.add_argument('--train_or_generate', '--tg', type=int, default=0, help='train is 0 : generete is 1')
-parser.add_argument('--test_size',type=int, default=1000, help='test_size')
 
-parser.add_argument('--result_path', type=str)
-parser.add_argument('--model_path', type=str)
-parser.add_argument('--save_path', type=str)
-parser.add_argument('--debug', type=int, default=0)
+parser.add_argument('--max_article_len', type=int, default=400,
+                    help='max article length')
+parser.add_argument('--max_summary_len', type=int, default=100,
+                    help='max summary length')
+'''
+related files
 
-parser.add_argument('--load_article_file', type=str, default="data/article_word.pt",
+    result_path
+    model_path
+    save_path
+    load_article_file
+    load_summary_file
+
+'''
+parser.add_argument('--result_path', '-r', type=str)
+parser.add_argument('--model_path', '-m', type=str)
+parser.add_argument('--save_path', '-s',  type=str)
+
+parser.add_argument('--load_article_file', type=str, default="data/article.pt",
                     help='load article file')
-parser.add_argument('--load_summary_file', type=str, default="data/summary_word.pt",
+parser.add_argument('--load_summary_file', type=str, default="data/summary.pt",
                     help='load article file')
-parser.add_argument('--create_data', type=int, default=0,
-                    help='if over 0 like 1 it will create data')
+parser.add_argument('--save_article_file', type=str, default="data/article.pt",
+                    help='save article file')
+parser.add_argument('--save_summary_file', type=str, default="data/summary.pt",
+                    help='save article file')
+parser.add_argument('--mode', type=str, default="dubug",
+                    help='save debug train evaluate')
 args = parser.parse_args()
-
 ##### end #####
 
-word_data = Word_Data()
-
-if args.create_data:
-    article_save_path = "data/article_word.pt"
-    summary_save_path = "data/summary_word.pt"
-    word_data.save(article_save_path, summary_save_path)
+vocab_path = os.environ['cnn_vocab50000']
+preprocess = Preprocess(args.max_article_len, args.max_summary_len)
+"""
+    source_target dict and size is same
+"""
+source_dict = preprocess.getVocab(vocab_path)
+target_dict = preprocess.getVocab(vocab_path)
+source_size = len(source_dict)
+target_size = len(target_dict)
+if args.mode == "save":
+    pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    train_src = '{}/{}'.format(pardir, "plain_data/train.txt.src")
+    train_tgt = '{}/{}'.format(pardir, "plain_data/train.txt.tgt.tagged")
+    print("source data path: {} ".format(train_src)) 
+    print("target data path: {} ".format(train_tgt)) 
+    train_source = preprocess.save(train_src , 0, source_dict, args.save_article_file, dubug=False)
+    train_target = preprocess.save(train_tgt , 1, target_dict, args.save_summary_file, dubug=False)
     exit()
 
-if args.debug:
-    train_doc_num = 6
+elif args.mode == "debug":
     hidden_size = 4
-    embed_size = 4
-    batch_size = 2
-    epoch = 2
+    embed_size = 2
+    max_epoch = 2
+    batch_size = 3
+    train_source = preprocess.load("data/dubug_article.pt")[:9]
+    train_target = preprocess.load("data/dubug_summary.pt")[:9]
 
-else:
-    train_doc_num = args.train_doc_num
+elif args.mode == "train":
     hidden_size = args.hidden_size
     embed_size = args.embed_size
+    max_epoch = args.epoch
     batch_size = args.batch_size
-    epoch = args.epoch
+    train_source = preprocess.load(args.load_article_file)
+    train_target = preprocess.load(args.load_summary_file)
 
-if args.train_or_generate == 1:
-    get_test_data_target(args.test_size, output_input_lines)
-
-source_size = word_data.getVocabSize() + 1
-target_size = source_size
-if train_doc_num is None:
-    article_data = torch.load(args.load_article_file)
-    summary_data = torch.load(args.load_summary_file)
-    train_doc_num = len(article_data)
-else:
-    article_data = torch.load(args.load_article_file)[0:train_doc_num]
-    summary_data = torch.load(args.load_summary_file)[0:train_doc_num]
-logger.debug("訓練文書数: " +  str(train_doc_num))
-logger.debug("hidden_size: " + str(hidden_size))
-logger.debug("embed_size: " +  str(embed_size))
-logger.debug("epoch : " + str(epoch))
-logger.debug("batch size : " +  str(batch_size))
+''' share '''
+dropout = args.dropout
+print("source document length : {} ".format(len(train_source)))
+print("target document length : {} ".format(len(train_target)))
+print("hidden_size: {} ".format(hidden_size))
+print("embed_size: {} ".format(embed_size))
+print("batch_size: {} ".format(batch_size))
+print("max epoch: {} ".format(max_epoch))
--- baseline/preprocessing.py
+++ baseline/preprocessing.py
@@ -6,70 +6,57 @@ import argparse
 from torch.utils.data.dataset import Dataset
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
-
+import copy
 PADDING = 0
 UNK = 1
-BOS = 2
-EOS = 3
+START_DECODING = 2
+STOP_DECODING = 3
 
-class MyDataset(Dataset):
-    def __init__(self, source, target):
-        self.source = source
-        self.target = target
+class Preprocess():
+    def __init__(self, max_article_len, max_summary_len):
+        self.init_dict = {"[PAD]": PADDING ,"[UNK]": UNK, "[START]": START_DECODING, "[STOP]": STOP_DECODING}
+        self.max_article_len = max_article_len
+        self.max_summary_len = max_summary_len
 
-    def __getitem__(self, index):
-        get_source = self.source[index]
-        get_target = self.target[index]
-        return [get_source, get_target]
+    def getVocab(self, vocab_file):
+        return self.pushVocab(vocab_file)
 
-    def __len__(self):
-        return len(self.source)
-
-    def collater(self, items):
-        source_items = [item[0] for item in items]
-        target_items = [item[1] for item in items]
-        source_padding = pad_sequence(source_items, batch_first=True)
-        target_padding = pad_sequence(target_items, batch_first=True)
-        return [source_padding, target_padding]
-
-class Word_Data():
-    def __init__(self):
-        self.vocab_file = os.environ['cnn_vocab50000']
-        self.dict = {"[UNK]": UNK, "[BOS]": BOS, "[EOS]": EOS}
-
-        pardir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        train_src = '{}/{}'.format(pardir, "plain_data/train.txt.src")
-        train_tgt = '{}/{}'.format(pardir, "plain_data/train.txt.tgt.tagged")
-        self.article_path = train_src
-        self.summary_path = train_tgt
-
-    def getVocabSize(self):
-        self.pushVocab()
-        return len(self.dict)
-
-    def save(self, article_file , summary_file):
-        self.pushVocab()
-        self.SaveTensorData(self.article_path, article_file)
-        self.SaveTensorData(self.summary_path, summary_file)
-
-    def pushVocab(self):
-        with open(self.vocab_file) as f:
+    def pushVocab(self, vocab_file):
+        vocab_dict = copy.copy(self.init_dict)
+        with open(vocab_file) as f:
             for count, vocab in enumerate(f):
-                self.dict[vocab.strip()] = len(self.dict) + 1
+                vocab_dict[vocab.strip()] = len(vocab_dict)
+                if len(vocab_dict) >= 50000:
+                    break
+        return vocab_dict
 
-    def SaveTensorData(self, path, file_name):
-        tensor_data = self.GetTensorData(path)
-        torch.save(tensor_data, file_name)
+    def load(self, save_file):
+        return torch.load(save_file)
 
-    def GetTensorData(self, file_path):
-        with open(file_path) as f:
-            tensor_data = [ self.ConvertTensor(doc) for i, doc in enumerate(f)]
-        return tensor_data
+    def save(self, data_path, mode, vocab_dict, save_file, debug=False):
+        '''
+            mode : 0 -> source
+            mode : 1 -> target
+        '''
+        self.dict = vocab_dict
+        with open(data_path) as data:
+            if debug:
+                tensor_data = [ self.ConvertTensor(doc, mode) for count , doc in enumerate(data) if count <= 20]
+            else:
+                tensor_data = [ self.ConvertTensor(doc, mode) for count , doc in enumerate(data)]
+        torch.save(tensor_data, save_file)
 
-    def ConvertTensor(self, doc):
+    def ConvertTensor(self, doc, mode):
+        '''
+            mode : 0 -> source
+            mode : 1 -> target
+        '''
         doc = self.replaceWord(doc)
-        words = self.DocToWord(doc)
-        words = ["[BOS]"] + words + ["[EOS]"]
+        if mode == 1:
+            words = doc.strip().split(' ')[:self.max_summary_len]
+            words = ["[START]"] + words + ["[STOP]"]
+        else:
+            words = doc.strip().split(' ')[:self.max_article_len]
         words_id = self.SentenceToDictID(words)
         return words_id
 
@@ -77,9 +64,6 @@ class Word_Data():
         doc = doc.replace("<t>", "")
         doc = doc.replace("</t>", "")
         return doc
-
-    def DocToWord(self, strs):
-        return strs.strip().split(' ')
 
     def SentenceToDictID(self, sentence):
         slist = []
