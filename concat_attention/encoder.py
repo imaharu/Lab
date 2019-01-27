Fix concat_attention masking

concat_attention/encoder.py
@@ -16,8 +16,12 @@ class Encoder(nn.Module):
     def forward(self, sentences):
         b = sentences.size(0)
         input_lengths = sentences.ne(0).sum(-1)
+        sorted_lengths, indices = torch.sort(input_lengths, descending=True)
+
+        sentences = sentences[indices]
+
         embed = self.embed(sentences)
-        sequence = rnn.pack_padded_sequence(embed, input_lengths, batch_first=True)
+        sequence = rnn.pack_padded_sequence(embed, sorted_lengths, batch_first=True)
 
         packed_output, (hx, cx) = self.lstm(sequence)
         encoder_outputs, _ = rnn.pad_packed_sequence(
@@ -27,6 +31,13 @@ class Encoder(nn.Module):
             encoder_outputs = encoder_outputs[:, :, :hidden_size] + encoder_outputs[:, :, hidden_size:]
             hx = hx.view(-1, 2 , b, hidden_size).sum(1)
             cx = cx.view(-1, 2 , b, hidden_size).sum(1)
+
+        inverse_indices = indices.sort()[1] # Inverse permutation
+        encoder_outputs = encoder_outputs[:, inverse_indices]
+
+        hx = hx[:, inverse_indices]
+        cx = cx[:, inverse_indices]
+
         encoder_features = self.W_h(encoder_outputs)
         hx = hx.view(b, -1)
         cx = cx.view(b, -1)