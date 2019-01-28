e54bcee4baa9c50d28d75f89d3c09213f092359f
imaharu <light1200out@gmail.com>
2019-01-28 12:45:31 +0900

    Fix generate

--- hred_coverage/define.py
+++ hred_coverage/define.py
@@ -23,6 +23,7 @@ parser.add_argument('--max_summary_len', type=int, default=100,
                     help='max summary length')
 
 parser.add_argument('--generate_dir', '-g' ,type=str, default="val")
+parser.add_argument('--result_file', '-r' ,type=str, default="rouge_result.txt")
 parser.add_argument('--model_path', '-m' , type=str)
 parser.add_argument('--save_dir', '-s' , type=str, default="train")
 
--- hred_coverage/exec_generate.py
+++ hred_coverage/exec_generate.py
@@ -27,3 +27,27 @@ optimizer.load_state_dict(checkpoint['optimizer'])
 
 generate_module = GenerateDoc(generate_data)
 save(model, generate_module)
+
+from pyrouge import Rouge155
+
+def EvaluateByPyrouge(generate_path, model_dir):
+    r = Rouge155()
+    r.system_dir = generate_path
+    r.model_dir = model_dir
+    r.system_filename_pattern = '(\d+).txt'
+    r.model_filename_pattern = 'gold_#ID#.txt'
+    output = r.convert_and_evaluate()
+    save_dir = "{}/{}".format("trained_model", args.save_dir)
+    rouge_result = "{}/{}".format(save_dir, args.result_file)
+    with open(rouge_result, "w") as f:
+        print(output, file=f)
+    output_dict = r.output_to_dict(output)
+    return output_dict["rouge_1_f_score"], output_dict["rouge_2_f_score"], output_dict["rouge_l_f_score"]
+
+model_dir = "/home/ochi/Lab/gold_summary/val_summaries"
+save_dir = "{}/{}".format("trained_model", args.save_dir)
+generate_dir = "{}/{}".format(save_dir , args.generate_dir)
+rouge1, rouge2, rougeL = EvaluateByPyrouge(generate_dir, model_dir)
+print("rouge1", rouge1)
+print("rouge2", rouge2)
+print("rougeL", rougeL)
