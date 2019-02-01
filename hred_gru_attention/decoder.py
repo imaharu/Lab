311f492d01e02a5bb0a31bea60e7396de668c3a9
imaharu <light1200out@gmail.com>
2019-02-01 16:57:01 +0900

    Create gru

---
+++ hred_gru_attention/decoder.py
@@ -0,0 +1,51 @@
+from define import *
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn
+
+class WordDecoder(nn.Module):
+    def __init__(self):
+        super(WordDecoder, self).__init__()
+        self.embed = nn.Embedding(target_size, embed_size, padding_idx=0)
+        self.gru = nn.GRUCell(embed_size, hidden_size)
+        self.linear = nn.Linear(hidden_size, target_size)
+
+    def forward(self, summary_words, w_hx):
+        embed = self.embed(summary_words)
+        w_hx = self.gru(embed, w_hx)
+        return w_hx
+
+class SentenceDecoder(nn.Module):
+    def __init__(self, opts):
+        super(SentenceDecoder, self).__init__()
+        self.gru = nn.GRUCell(hidden_size, hidden_size)
+        self.attention = Attention(opts)
+
+    def forward(self, w_hx, s_hx, encoder_outputs, encoder_features, mask_tensor):
+        s_hx = self.gru(w_hx, s_hx)
+        final_dist = self.attention(
+                s_hx, encoder_outputs, encoder_features, mask_tensor)
+        return final_dist, s_hx
+
+class Attention(nn.Module):
+    def __init__(self, opts):
+        super(Attention, self).__init__()
+        self.opts = opts
+        self.W_s = nn.Linear(hidden_size, hidden_size)
+        self.v = nn.Linear(hidden_size, 1)
+        self.linear = nn.Linear(hidden_size * 2, hidden_size)
+
+    def forward(self, decoder_hx, encoder_outputs , encoder_feature , mask_tensor):
+        t_k, b, n = list(encoder_outputs.size())
+        dec_feature = self.W_s(decoder_hx)
+        dec_feature = dec_feature.unsqueeze(0).expand(t_k, b, n)
+        att_features = encoder_feature + dec_feature
+        e = torch.tanh(att_features)
+        scores = self.v(e)
+        attn_dist = torch.softmax(scores, dim=0) * mask_tensor # sen_len x Batch x 1
+
+        content_vector = (attn_dist * encoder_outputs).sum(0)
+        concat = torch.cat((content_vector, decoder_hx), 1)
+        final_dist = torch.tanh(self.linear(concat))
+        return final_dist
---
+++ hred_gru_attention/encoder.py
@@ -0,0 +1,77 @@
+from define import *
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn
+
+class WordEncoder(nn.Module):
+    def __init__(self, opts):
+        super(WordEncoder, self).__init__()
+        self.opts = opts
+        self.embed = nn.Embedding(source_size, embed_size, padding_idx=0)
+        self.lstm = nn.GRU(embed_size, hidden_size, batch_first=True, bidirectional=self.opts["bidirectional"])
+
+    def forward(self, sentences):
+        except_flag = False
+        b = sentences.size(0)
+
+        input_lengths = sentences.ne(0).sum(-1)
+        sorted_lengths, indices = torch.sort(input_lengths, descending=True)
+        sentences = sentences[indices]
+        masked_select = sorted_lengths.masked_select(sorted_lengths.ne(0))
+
+        # if all 0 sentence is appeared
+        if not torch.equal(sorted_lengths, masked_select):
+            s_b = b
+            b = masked_select.size(0)
+            sentences = sentences.narrow(0, 0, b)
+            sorted_lengths = masked_select
+            except_flag = True
+
+        embed = self.embed(sentences)
+        sequence = rnn.pack_padded_sequence(embed, sorted_lengths, batch_first=True)
+        _, w_hx = self.lstm(sequence)
+
+        if self.opts["bidirectional"]:
+            w_hx = w_hx.view(-1, 2 , b, hidden_size).sum(1)
+        w_hx = w_hx.view(b , -1)
+
+        if except_flag:
+            zeros = torch.zeros((s_b - b, hidden_size)).cuda()
+            w_hx = torch.cat((w_hx, zeros), 0)
+
+        inverse_indices = indices.sort()[1] # Inverse permutation
+        w_hx = w_hx[inverse_indices]
+        return w_hx
+
+class SentenceEncoder(nn.Module):
+    def __init__(self, opts):
+        super(SentenceEncoder, self).__init__()
+        self.opts = opts
+        self.lstm = nn.GRU(hidden_size, hidden_size, bidirectional=self.opts["bidirectional"])
+        self.W_h = nn.Linear(hidden_size, hidden_size)
+
+    def forward(self, words_encoder_outputs):
+        b = words_encoder_outputs.size(1)
+        input_sentences = words_encoder_outputs.transpose(0,1)
+        input_lengths = input_sentences.ne(0).sum(1).t()[0]
+
+        sorted_lengths, indices = torch.sort(input_lengths, descending=True)
+        input_sentences = input_sentences[indices]
+
+        sequence = rnn.pack_padded_sequence(input_sentences, sorted_lengths, batch_first=True)
+        sentence_outputs, s_hx = self.lstm(sequence)
+        sentence_outputs, _ = rnn.pad_packed_sequence(
+            sentence_outputs
+        )
+
+        inverse_indices = indices.sort()[1] # Inverse permutation
+        sentence_outputs = sentence_outputs[:, inverse_indices]
+        s_hx = s_hx[:, inverse_indices]
+
+        if self.opts["bidirectional"]:
+            sentence_outputs = sentence_outputs[:, :, :hidden_size] + sentence_outputs[:, :, hidden_size:]
+            s_hx = s_hx.view(-1, 2 , b, hidden_size).sum(1)
+        sentence_features = self.W_h(sentence_outputs)
+        s_hx = s_hx.view(b, -1)
+        return sentence_outputs, sentence_features, s_hx
